3f707b4d7fd6b74b06b5f5f00ea231f1d75a8d42
JC Coles <jcoles1155@gmail.com>
2024-03-03 21:35:34 -0800

    finally

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,11 +16,10 @@ export default function Hero() {
             alt="Me"
           />
           <h1 className="h1 font-aspekta mb-5">
-            I write{" "}
             <span className="inline-flex relative text-sky-500 before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:-rotate-2 before:translate-y-1/4">
-              code, music,
+              code, surf, music,
             </span>{" "}
-            and <strong>occassionally</strong> throw in some op-ed.
+            and <strong>occassionally</strong> some op-ed.
           </h1>
           <p className="text-lg text-slate-500 dark:text-slate-400">
             You will usually find me trekking the PCH looking for a wave,
