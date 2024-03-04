53e27cfe807d619193d0017a866a13040bccf991
JC Coles <jcoles1155@gmail.com>
2024-03-03 21:08:27 -0800

    oh yeah

--- components/talks.tsx
+++ components/talks.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Talk01 from "@/public/images/cargo-cult-video-otw.png";
+import Talk01 from "@/public/images/9-metrics-investors-want.png";
 import Talk02 from "@/public/images/open-ai-demo-day.png";
 
 export default function Talks() {
@@ -13,7 +13,7 @@ export default function Talks() {
       <div className="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-5">
         <a
           className="relative aspect-video rounded-lg overflow-hidden bg-gradient-to-tr from-slate-800 to-slate-700 odd:rotate-1 even:-rotate-1 hover:rotate-0 transition-transform duration-700 hover:duration-100 ease-in-out shadow-xl"
-          href="https://www.youtube.com/watch?v=ECLJ95XNxA4"
+          href="https://www.youtube.com/embed/PTg3RZPXgLg"
           rel="noreferrer"
           target="_blank"
         >
@@ -26,7 +26,7 @@ export default function Talks() {
           />
           <div className="h-full relative flex flex-col items-start justify-between before:mt-auto before:flex-1 p-5">
             <div className="flex-1 flex items-center text-lg font-aspekta text-white font-[650]">
-              What is Cargo Culting?
+              9 metrics Investors want
             </div>
             <div className="flex-1 w-full flex justify-end items-end">
               <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41">
