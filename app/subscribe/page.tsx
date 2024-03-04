522c0a178c4adbda610e742e31212e005c77dfff
JC Coles <jcoles1155@gmail.com>
2024-03-03 20:55:00 -0800

    :)

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -58,7 +58,7 @@ export default function Subscribe() {
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
                     <span>
-                      Actionable advice for engineerings, and tech teams.
+                      Actionable advice for engineers, and tech teams.
                     </span>
                   </li>
                   <li className="flex items-start">
@@ -70,9 +70,7 @@ export default function Subscribe() {
                     >
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
-                    <span>
-                      A pulse on the tech market and scoop on emerging tech.
-                    </span>
+                    <span>Scoop on emerging tech.</span>
                   </li>
                 </ul>
                 <div>
