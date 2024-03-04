c69aabcdcfa3a30f9ade6d0ef0fb4ead2416dd8f
JC Coles <jcoles1155@gmail.com>
2024-03-03 18:44:01 -0800

    all clean

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import HeroImage from "@/public/images/sis_and_bro.jpg";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -15,15 +16,33 @@ export default function Hero() {
             alt="Me"
           />
           <h1 className="h1 font-aspekta mb-5">
-            I write about code and being a{" "}
+            I write code, music, and{" "}
             <span className="inline-flex relative text-sky-500 before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:-rotate-2 before:translate-y-1/4">
-              full-time
+              occassionally
             </span>{" "}
-            maker.
+            throw in some op-ed.
           </h1>
           <p className="text-lg text-slate-500 dark:text-slate-400">
-            Full Stack developer: surfing, traveling, and working on apps. I
-            write about coding, startups, and my journey as a maker.
+            You will usually find me trekking the PCH looking for a wave,
+            writing tunes with friends, or posting up with the starlink at a
+            campsite.
+          </p>
+          <br />
+          <p>
+            Hit me up on{" "}
+            {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              <a
+                href={"https://www.linkedin.com/in/nicholas-jc-coles-314495a6/"}
+              >
+                the socials{" "}
+              </a>
+            </button>{" "}
+            or{" "}
+            <Link href={"/subscribe"}>
+              <strong>schedule a call</strong>
+            </Link>{" "}
+            to chat!
           </p>
         </div>
       </div>
